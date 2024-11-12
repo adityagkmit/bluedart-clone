eecffb5dea811b6d038fc6e4db17f8df8baa6f1c
Aditya Singh Rajawat <adityarajawat@gkmit.co>
2024-11-12 15:33:39 +0530

    feat: update role middleware for specific use

--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -2,21 +2,30 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/users.controller');
 const { auth } = require('../middlewares/auth.middleware');
-const allowedRoles = require('../middlewares/role.middleware');
+const roles = require('../middlewares/role.middleware');
 const validate = require('../middlewares/validator.middleware');
 const { userIdValidateSchema, createUserSchema, updateUserSchema } = require('../validators/users.validator');
 
 // Routes for user operations
-router.get('/', auth, allowedRoles(['Admin']), userController.getAllUsers);
-router.post('/', auth, allowedRoles(['Admin']), validate(createUserSchema), userController.createUser);
+router.get('/', auth, roles(), userController.getAllUsers);
+router.post('/', auth, roles(), validate(createUserSchema), userController.createUser);
 router.get('/:id', auth, validate(userIdValidateSchema, true), userController.getUserById);
+
 router.put(
   '/:id',
   auth,
+  roles(['Admin'], true),
   validate(updateUserSchema),
   validate(userIdValidateSchema, true),
   userController.updateUser
 );
-router.delete('/:id', auth, validate(userIdValidateSchema, true), userController.deleteUser);
+
+router.delete(
+  '/:id',
+  auth,
+  roles(['Admin'], true),
+  validate(userIdValidateSchema, true),
+  userController.deleteUser
+);
 
 module.exports = router;
